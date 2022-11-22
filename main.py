ba0289e5934681f142d83c73575348c05cdc0724
Toby <newtobycm@gmail.com>
2022-11-21 21:24:45 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Main module file
+"""
+
+# from PIL import Image, ImageDraw, ImageOps
+
+from qrcode import QRCode, ERROR_CORRECT_H
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+
+def generate_qr_code(input_data):
+
+    qr = QRCode(
+        version = None,
+        error_correction = ERROR_CORRECT_H,
+        box_size = 10,
+        border = 5
+    )
+    qr.add_data(input_data)
+    qr.make(fit = True)
+
+    qr_code = qr.make_image(image_factory = StyledPilImage, module_drawer = RoundedModuleDrawer())
+
+    # mask = Image.new("L", (qr_code.pixel_size, qr_code.pixel_size), 0)
+    # draw = ImageDraw.Draw(mask) 
+    # draw.ellipse((qr_code.pixel_size, qr_code.pixel_size) + (150, 150), fill=255)
+    # mask = ImageOps.fit(mask, (qr_code.pixel_size, qr_code.pixel_size), centering = (0.5, 0.5))
+
+    # qr_code.putalpha(mask)
+
+    return qr_code
