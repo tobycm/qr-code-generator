361dfe678e17db88e8731aa1af12824867476941
Toby <newtobycm@gmail.com>
2022-11-22 13:08:33 -0800

    Small changes with type hint

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ Main module file
 """
 
 # from PIL import Image, ImageDraw, ImageOps
+from PIL import Image
 
 from qrcode import QRCode, ERROR_CORRECT_H
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
 
-def generate_qr_code(input_data):
+def generate_qr_code(input_data) -> Image:
+    """
+    Generate and return a qr code in PILImage form.
+    """
 
     qr = QRCode(
         version = None,
@@ -22,7 +26,7 @@ def generate_qr_code(input_data):
     qr_code = qr.make_image(image_factory = StyledPilImage, module_drawer = RoundedModuleDrawer())
 
     # mask = Image.new("L", (qr_code.pixel_size, qr_code.pixel_size), 0)
-    # draw = ImageDraw.Draw(mask) 
+    # draw = ImageDraw.Draw(mask)
     # draw.ellipse((qr_code.pixel_size, qr_code.pixel_size) + (150, 150), fill=255)
     # mask = ImageOps.fit(mask, (qr_code.pixel_size, qr_code.pixel_size), centering = (0.5, 0.5))
 
